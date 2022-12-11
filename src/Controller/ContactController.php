e7349d4856a40e80d5097013ab3fa7f1cfd912eb
Hamza Benketaf <hamza.benketaf@gmail.com>
2022-12-11 23:59:30 +0100

    test

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -36,7 +36,7 @@ class ContactController extends AbstractController
 
       $email = (new TemplatedEmail())
         ->from($contact->getEmail())
-        ->to('admin@nb-facialiste.fr');
+        ->to('hamza.benketaf@gmail.com');
       if ($contact->getSujet() !== null) {
         $email->subject($contact->getSujet());
       }
